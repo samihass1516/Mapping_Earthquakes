c8d04c95ad6f75f0667871d8ec7a4d344623970c
samihass1516 <114379268+samihass1516@users.noreply.github.com>
2023-01-18 23:30:30 -0500

    Adding files for Earthequake_past7days

---
+++ Earthquakes_past7day/static/js/cities.js
@@ -0,0 +1,8 @@
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location).addTo(map);
+});
